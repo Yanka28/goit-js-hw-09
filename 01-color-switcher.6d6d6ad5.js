654a9eadaf0183f8300c52aee5d490634c901e7e
Yanka28 <125182038+Yanka28@users.noreply.github.com>
2023-07-19 16:32:11 +0000

    Deploying to gh-pages from @ Yanka28/goit-js-hw-09@aa0cd986932cb0a28e871b5a6922ba195aada964 🚀

--- 01-color-switcher.6d6d6ad5.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");let o=null;function a(t){e.disabled=t,n.disabled=!t}e.addEventListener("click",(function(){a(!0),o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),n.addEventListener("click",(function(){a(!1),clearInterval(o)}));
-//# sourceMappingURL=01-color-switcher.6d6d6ad5.js.map
